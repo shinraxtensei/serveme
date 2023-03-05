c582ca2caf4a91abf720055d366c6280fce3e248
shinraxtensei <chinraxtensei@gmail.com>
2023-03-05 13:41:00 +0100

    the server now is able to handle disconnected clients , we need to handle req and res

--- srcs/core.cpp
+++ srcs/core.cpp
@@ -57,7 +57,7 @@ void Core::startup()
 
 
 
-void Core::HandleResquest(int fd)
+void Core::HandleResquest(pollfd FD)
 {
     // TODO : we need to parse the request in order to check for the servername to see which server should handle the request
     // TODO : 
@@ -66,7 +66,7 @@ void Core::HandleResquest(int fd)
     std::string request;
     char buf[1024];
     int ret = 0;
-    while ((ret = recv(fd, buf, 1024, 0)) > 0)
+    while ((ret = recv(FD.fd, buf, 1024, 0)) > 0)
     {
         if (ret == -1 || ret == 0)
         {
@@ -90,7 +90,7 @@ void Core::HandleResquest(int fd)
     std::string headers = "Content-Type: text/html\r\n";
     std::string body = "<html><body><h1>Hello, world!</h1></body></html>";
     std::string response = status + headers + "\r\n" + body;
-    send(fd, response.c_str(), response.size(), 0);
+    send(FD.fd, response.c_str(), response.size(), 0);
 
 }
 
@@ -100,8 +100,8 @@ void Core::HandleResquest(int fd)
 void Core::handleConnections()
 {
     
-    // TODO : make the clients able to disconnect 
-    // TODO : the core should handle the creation and binding of sockets , and it should be able to give to the server to handle the appropriate request , depending on the servername/default server 
+//     // TODO : make the clients able to disconnect 
+//     // TODO : the core should handle the creation and binding of sockets , and it should be able to give to the server to handle the appropriate request , depending on the servername/default server 
 
 
     std::vector<pollfd> pollFds;
@@ -113,53 +113,113 @@ void Core::handleConnections()
         pollFds.push_back(fd);
     }
 
-    while(true)
-    {
-        int ret = poll(pollFds.data(), pollFds.size(), 10);
-        if (ret == -1)
-        {
-            std::cerr << "Error: poll() failed" << std::endl;
-            exit(1);
-        }
+//     while(true)
+//     {
+//         int ret = poll(pollFds.data(), pollFds.size(), 10);
+//         if (ret == -1)
+//         {
+//             std::cerr << "Error: poll() failed" << std::endl;
+//             exit(1);
+//         }
 
-        for (size_t i = 0; i < pollFds.size() ; i++)
-        {
-            if (pollFds[i].revents & POLLIN)
+//         for (size_t i = 0; i < pollFds.size() ; i++)
+//         {
+//             if (pollFds[i].revents & POLLIN)
+//             {
+//                 if (int it = check_servers_socket(pollFds[i].fd) ; it != -1)
+//                 {
+//                     std::cout << "new connection\n" ;
+
+
+
+//                     this->clients.push_back(Client(this->serverSockets[it]));
+//                     pollFds.push_back(this->clients.back().pollfd_);
+
+//                 }
+//                 else
+//                 {   
+//                     std::cout << "request\n";
+//                     char buf[1024];
+//                     recv(pollFds[i].fd, buf, 1024, 0);
+//                     // read(pollFds[i].fd, buf, 1);
+
+//                     break;
+//                 }
+//             }
+//             // else if (pollFds[i].revents & POLLOUT)
+//             // {
+//             //     std::cout << "POLLOUT\n";
+//             //     exit(0);
+//             // }
+//             // else if (pollFds[i].revents & POLLHUP)
+//             // {
+
+//             //     std::cout << "client disconnected \n";
+//             //     pollFds[i].fd = -1;
+
+//             // }
+
+
+//         }
+//     }
+
+
+while (true) {
+    int ret = poll(pollFds.data(), pollFds.size(), 10);
+    if (ret == -1) {
+        std::cerr << "Error: poll() failed" << std::endl;
+        exit(1);
+    }
+
+    for (size_t i = 0; i < pollFds.size(); i++) {
+        if (pollFds[i].revents & POLLIN) {
+            if (int it = check_servers_socket(pollFds[i].fd); it != -1) 
             {
-                if (int it = check_servers_socket(pollFds[i].fd) ; it != -1)
-                {
-                    std::cout << "new connection\n" ;
-
-
-
-                    this->clients.push_back(Client(this->serverSockets[it]));
-                    pollFds.push_back(this->clients.back().pollfd_);
-
-                }
-                else
-                {   
-                    char buf[2];
-                    recv(pollFds[i].fd, buf, 2, 0);
-                    std::cout << "request\n";
-                    Core::HandleResquest(pollFds[i].fd);
-                    break;
+                // New connection
+                std::cout << "New connection\n";
+                this->clients.push_back(Client(this->serverSockets[it]));
+                pollFds.push_back(this->clients.back().pollfd_);
+            } 
+            else {
+                // Client request
+                std::cout << "Client request\n";
+                char buf[1024];
+                int bytes_received = recv(pollFds[i].fd, buf, 1024, 0);
+                if (bytes_received == -1) {
+                    // Error
+                    std::cerr << "Error: recv() failed" << std::endl;
+                    close(pollFds[i].fd);
+                    pollFds.erase(pollFds.begin() + i);
+                    i--;
+                } 
+                else if (bytes_received == 0) {
+                    // Client disconnected
+                    std::cout << "Client disconnected\n";
+                    close(pollFds[i].fd);
+                    pollFds.erase(pollFds.begin() + i);
+                    i--;
+                } 
+                else {
+                    std::cout << "Request: " <<  buf << std::endl;
+                    send(pollFds[i].fd, "<html> <h1> hello </h1> </html>", 5, 0);
+                    // Parse request and send response
+                    // std::string request(buf, bytes_received);
+                    // std::string response = handle_request(request);
+                    // send(pollFds[i].fd, response.c_str(), response.length(), 0);
+                    // std::cout << "Request: " << request << std::endl;
+                    // request = "Request : " + request;
+                    // send(pollFds[i].fd,request.c_str(), 5, 0);
                 }
             }
-            // else if (pollFds[i].revents & POLLOUT)
-            // {
-            //     std::cout << "POLLOUT\n";
-            //     exit(0);
-            // }
-            else if (pollFds[i].revents & POLLHUP)
-            {
-
-                std::cout << "client disconnected \n";
-                close(pollFds[i].fd);
-                pollFds.erase(pollFds.begin() + i);
-                exit(0);
-            }
-
-
         }
     }
 }
+
+
+
+
+
+}
+
+
+
